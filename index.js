615f5f03c46da2434b26cda31e7c29b9f4514155
atok624 <84607674+atok624@users.noreply.github.com>
2023-02-15 14:11:18 +0000

    Modify index.js file

--- index.js
+++ index.js
@@ -67,47 +67,6 @@ function Display(item) {
   ul.appendChild(li);
 }
 
-// function createBooks(arr) {
-//   let books = '';
-//   for (let i = 0; i < arr.length; i += 1) {
-//     books += `
-//           <p>${arr[i].title}</p>
-//           <p>${arr[i].author}</p>
-//           <button onclick = "removeBook(${i})">Remove</button>
-//           <hr />
-//           `;
-//   }
-//   return books;
-// }
-
-// // Displaying data to the UI from local storage
-// function displayBooks() {
-//   const listOfBooks = document.querySelector('.container');
-//   listOfBooks.innerHTML = `
-//       <ul class="book-ul">
-//       ${createBooks(storeData)}</ul>
-//       `;
-// }
-
-// // Adding new data in the local storage
-// function addNewdata(bookTitle, bookAuthor) {
-//   const Book = {
-//     title: bookTitle,
-//     author: bookAuthor,
-//   };
-//   storeData.push(Book);
-//   updateData();
-//   displayBooks();
-// }
-
-// Removing data from local storage
-// function removeBook(i) {
-//   storeData.splice(i, 1);
-//   updateData();
-//   displayBooks();
-// }
-
-// window.onload = Display();
 window.onload = () => {
   storeBook.books = JSON.parse(localStorage.getItem('Added Books' || '[]'));
   if (storeBook.books === null) {
